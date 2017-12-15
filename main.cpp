b570904f450433315f6fefad3a66d7f2bf954c18
eric <849931616@qq.com>
2017-12-15 21:59:52 +0800

    add linklist.h

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "staticlist.h"
+#include "linklist.h"
 
 using namespace ELibTest;
 using namespace std;
@@ -7,15 +7,20 @@ using namespace std;
 class a
 {
 
+public:
+    a()
+    {
+        throw 0;
+    }
 
 };
 
 int main(void)
 {
-    StaticList<a,5> t,t2;
-    a n;
-    t.insert(n);
-    t.insert(n);
-    t2 = t;
+   LinkList<int> l;
+   l.insert(0,1);
+   l.insert(3);
+   l.insert(33);
+    cout<<l.find(3)<<endl;
     return 0;
 }
